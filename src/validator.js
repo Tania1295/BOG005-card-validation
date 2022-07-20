d3fe726faf8b3d685932fd87600a8f6df43f29ab
Tania1295 <108893226+Tania1295@users.noreply.github.com>
2022-07-20 16:04:50 -0500

    Cambios de ubicación

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,17 @@
 const validator = {
   // ...
+isValid: function ValidateSomething(){
+    // Check if your textbox is empty
+    if(document.getElementById('example-textbox').value.length == 0){
+         alert("Your textbox is empty. Please type something...");
+         document.getElementById('example-textbox').focus();
+         return false;
+    }
+    else{
+         // Everything is valid
+         alert('Your form is valid! (Do something)');
+         return true;
+    }
+}
 };
-
 export default validator;
