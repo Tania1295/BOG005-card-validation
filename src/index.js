42e2bec5da03a8585f94c6f3b4bbe21ab410dc80
Tania1295 <108893226+Tania1295@users.noreply.github.com>
2022-07-22 09:54:11 -0500

    Ajustes 22 07

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import validator from './validator.js';
-//console.log(validator);
+console.log("creditCardNumber");
 
 var btn = document.getElementById("validateTc");
-var creditCardNumber = document.getElementById("numeroTc");
+//var creditCardNumber = document.getElementById("numeroTc");
 var result;
 
 btn.onclick = function almacenar(){
-    creditCardNumber = creditCardNumber.value;
+    var creditCardNumber = document.getElementById("numeroTc").value;
+//    creditCardNumber = creditCardNumber.value;
     validator.isValid(creditCardNumber);
-    var result2 = validator.maskify(creditCardNumber);
-    if(validator.isValid(creditCardNumber)==true){
-        result = "valida"
+    validator.maskify(creditCardNumber);
+    if(validator.isValid(creditCardNumber) == true){
+        result = "válida"
+        console.log("result");
     }else{
-        result = "invalida"
+        result = "inválida"
     }
-    alert("Su tarjeta " + result2 + "es" + result);
+    alert("Su tarjeta " + " " + creditCardNumber + " es " + result);
     return false;
 }
--- src/validator.js
+++ src/validator.js
@@ -2,31 +2,34 @@ const validator ={
      isValid: function (creditCardNumber) {
        var numberTc = new Array ();
        var suma = 0;
-       var total;
-       var validate;
-       //Separar el array en elementos individuales
-       numberTc = creditCardNumber.split("");
-       numberTc.reverse();
 
-       for(var i =1; i< numberTc.length ; i += 2){
+    //Separar el array en elementos individuales
+       numberTc = creditCardNumber.split();
+       console.log(typeof creditCardNumber);
+       //Se reversa el array el último es el primero el primero es el último
+       numberTc.reverse();
+       //Función para realizar multiplicación posiciones pares y suma para números mayores de 9, la const guardarNumeros almacena los datos ingresados
+       const guardarNumeros = [ ]
+       for(var i = 1; i < numberTc.length ; i += 2){
         if(numberTc[i]<5){
-            numberTc[i] = numberTc [i] * 2;
+            guardarNumeros.push(numberTc [i] * 2)
         }
         else
        {
-        numberTc[i]= ((numberTc[i]- 5)* 2) + 1;
+        guardarNumeros.push(((numberTc[i]- 5)* 2) + 1)
        }
     }
-    for (var j =0; j< numberTc.length; j++){
-        suma += Number(numberTc[j]);
-
-        total= suma % 10;
-
-        validate = total == 0;
-
-        return validate;
+    //Función para verificar el resultado de la suma que sea producto igual a 0, v = valor obtenido en guardarNumeros
+    for (var v =0; v < guardarNumeros.length; v++){
+        suma += Number(guardarNumeros[v]);
+    }
+    if (suma % 10 === 0){
+        return true;
+    } else{
+        return false;
     }
     },
+    //Función para ocultar los números de la TC
 maskify: function(creditCardNumber){
     var bloque = "#";
     var hideTcNumber =(creditCardNumber.slice(0, -4).replace(/./g, bloque)+("" + creditCardNumber.slice(-4)));
