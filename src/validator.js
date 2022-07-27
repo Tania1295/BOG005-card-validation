7fa2891b4f1cd8e7b6b4d385af7d4908e8769da3
Tania1295 <108893226+Tania1295@users.noreply.github.com>
2022-07-26 22:03:41 -0500

    Función IsValid correcta

--- src/validator.js
+++ src/validator.js
@@ -1,12 +1,12 @@
 const validator = {
   isValid: (creditCardNumber) => {
-    debugger;
+    //    debugger;
     if (creditCardNumber === '') {
       return false;
     }
     let arr = creditCardNumber.split("").reverse(); //Separar el array en elementos individuales, revertirlo
     let suma = 0;
-    console.log('arr inicial', arr);
+    // console.log('arr inicial', arr);
 
     //Función para realizar multiplicación posiciones pares
     const numPositionPar = [];
@@ -14,35 +14,35 @@ const validator = {
       const datos = parseInt(arr[i])
       i % 2 != 0 ? numPositionPar.push(datos * 2) : numPositionPar.push(datos)
     }
-    console.log('paso 1', numPositionPar)
+    //console.log('paso 1', numPositionPar)
 
     // A las posiciones pares de numPositionPar que sean mayores a 9 restarle 9
-    for (let i = 1; i < numPositionPar.length; i = i + 2) {
+    for (let i = 0; i < numPositionPar.length; i++) {
       if (numPositionPar[i] > 9) {
         numPositionPar[i] = numPositionPar[i] - 9
       }
-      console.log('paso 2', numPositionPar)
+      // console.log('paso 2', numPositionPar)
 
       suma += numPositionPar[i]
-      console.log("paso 3", suma)
+      //console.log("paso 3", suma)
     }
     if (suma % 10 === 0) {
       return true;
-    }else{
+    } else {
       return false
     }
   }
   //Función para ocultar los números de la TC
-/*    maskify: (creditCardNumber) => {
-if (creditCardNumber.length >4){
-  const numeros = creditCardNumber.split("")
-  for(let init = 0; init < numeros.length - 4; init++){
-numeros[init] = "#"
-  }
-  return numeros.join("")
-}else{
-  return creditCardNumber
-}
-}, */
+   maskify: (creditCardNumber) => {
+    if (creditCardNumber.length > 4) {
+      const numeros = creditCardNumber.split("")
+      for (let init = 0; init < numeros.length - 4; init++) {
+        numeros[init] = "#"
+      }
+      return numeros.join("")
+    } else {
+      return creditCardNumber
+    }
+  },
 };
 export default validator
